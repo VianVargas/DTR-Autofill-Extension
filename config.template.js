c5c4361ea0dbca3220da749b257ba0144828b27b
vian <vianvargas43@gmail.com>
2026-04-20 23:22:09 +0800

    Add config template

---
+++ config.template.js
@@ -0,0 +1,7 @@
+// config.js
+// Instructions: Rename this file to "config.js" and fill in your actual details below.
+const DTR_CONFIG = {
+  fullName: "Lastname, Firstname",
+  eid: "123456",
+  mentorName: "MentorLast, MentorFirst",
+};
